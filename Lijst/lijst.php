8dbb7064ab01a367aea3aa0bfe1f65def317b25e
GijsJackersPXL <gijs.jackers@student.pxl.be>
2022-05-12 08:43:12 +0200

    p

--- Lijst/lijst.php
+++ Lijst/lijst.php
@@ -9,28 +9,27 @@
     if ($conn -> connect_error){
         die("connection failed: " . $conn->connect-error);
     }
+    
+    $sql = "SELECT AVG(WaardeSensor) FROM IOT_GEGEVENS WHERE SensorID = 1";
+    
+    //$result = mysql_query("SELECT AVG(fieldName) AS avg FROM tableName");
+    $row = mysql_fetch_assoc($sql);
+    echo $row;
 
-    $SensorID = "1";
 
-    $sql = "SELECT SensorID, WaardeSensor, DatumUpload FROM IOT_GEGEVENS WHERE SensorID = ?";
+
+/*     $sql = "SELECT SensorID, WaardeSensor, DatumUpload FROM IOT_GEGEVENS WHERE SensorID = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $SensorID);
+    $stmt->bind_param("s", $_GET['SensorID']);
     $stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($SensorID, $WaardeSensor, $DatumUpload);
-
+    
     while($row = $stmt->fetch())
     {
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>SensorID</th>";
-        echo "<td>" . $SensorID . "</td>";
-        echo "<th>DatumUpload</th>";
-        echo "<td>" . $DatumUpload . "</td>";
-        echo "<th>WaardeSensor</th>";
-        echo "<td>" . $WaardeSensor . "</td>";
-        echo "</tr>";	
+        echo "DatumUpload: ", $DatumUpload, "<br>";
+        echo "WaardeSensor: ", $WaardeSensor, "<br> <br>";
     }
-    echo "</table>";
     $stmt->close();
+ */
 ?>
